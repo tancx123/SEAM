c9175e43f6629853da4d8e3102aab977935d8361
tancx123 <tancx-pm17@student.tarc.edu.my>
2020-08-02 16:00:42 +0800

    Lasted Commit with add line item invoice

--- app/src/main/java/com/example/seam/ui/invoices/add_new_invoice.java
+++ app/src/main/java/com/example/seam/ui/invoices/add_new_invoice.java
@@ -2,15 +2,36 @@ package com.example.seam.ui.invoices;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.seam.R;
 
 public class add_new_invoice extends AppCompatActivity {
 
+    private Button BtnAddLine;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_invoice);
+
+        BtnAddLine = findViewById(R.id.add_line_item_btn);
+
+        BtnAddLine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddLineItem();
+            }
+        });
+
     }
+
+    public void openAddLineItem(){
+        Intent intent = new Intent(this, add_line_item.class);
+        startActivity(intent);
+    }
+
 }
