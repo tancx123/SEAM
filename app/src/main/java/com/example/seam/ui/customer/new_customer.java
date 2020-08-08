a52ec42922bd434a95de8c06e68975ee43f6fd97
tancx123 <tancx-pm17@student.tarc.edu.my>
2020-08-08 15:11:44 +0800

    Lasted Commit with add new customer

---
+++ app/src/main/java/com/example/seam/ui/customer/new_customer.java
@@ -0,0 +1,16 @@
+package com.example.seam.ui.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.seam.R;
+
+public class new_customer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_customer);
+    }
+}
