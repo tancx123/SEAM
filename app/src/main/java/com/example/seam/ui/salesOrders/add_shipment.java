162fc8d9ec11e7c1785eaf7b3535c503f79edbc2
tancx123 <tancx-pm17@student.tarc.edu.my>
2020-08-10 19:31:16 +0800

    Lasted Commit with add package

---
+++ app/src/main/java/com/example/seam/ui/salesOrders/add_shipment.java
@@ -0,0 +1,16 @@
+package com.example.seam.ui.salesOrders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.seam.R;
+
+public class add_shipment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_shipment);
+    }
+}
